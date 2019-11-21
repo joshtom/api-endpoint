a8199e2b99b8bd399240ebbf92d62258bfd8c56c
joshtom <olajidejoshua4real@gmail.com>
2019-11-21 12:22:11 +0100

    Update api.php

--- api.php
+++ api.php
@@ -1,9 +1,11 @@
 <?php
+
+error_reporting(1);
 $db_host = "localhost";
 $db_name = "api";
 $db_user = "root";
-$db_pass = "pass";
+$db_pass = "root";
 
-$connection = mysqli_connect($db_host,$db_name,$db_user,$db_pass);
+$connection = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die("No connection :".mysqli_error());
 
 ?>
