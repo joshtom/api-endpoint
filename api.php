8203d5b16422c66780027e1fd3952335386339d1
joshtom <olajidejoshua4real@gmail.com>
2019-11-21 12:25:48 +0100

    Created endpoint for login

--- api.php
+++ api.php
@@ -8,4 +8,16 @@ $db_pass = "root";
 
 $connection = mysqli_connect($db_host,$db_user,$db_pass,$db_name) or die("No connection :".mysqli_error());
 
+if(isset($_POST['ok-login'])) {
+    $username = trim(addslashes($_POST['username']));
+    $password = trim(addslashes($_POST['password']));
+
+    $query = "SELECT * FROM users WHERE username = '$username' && password = '$password";
+
+    $sql = mysqli_query($connection,$query);
+    $rows = mysqli_num_rows($sql);
+
+    var_dump($rows);
+}
+
 ?>
